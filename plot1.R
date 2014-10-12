a9db93dab63a6cd8084563d5e39036ce99e1c6c6
Dipanjan Biswas <dipanjan@dipanjan-M68M-S2P.(none)>
2014-10-12 19:18:30 +0530

    Corrected errors

--- plot1.R
+++ plot1.R
@@ -2,6 +2,6 @@ library(graphics)
 png("plot1.png")
 pcdata <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
 pcdsub <- pcdata[which((as.Date("2007-01-31") < as.Date(pcdata[,1],format="%d/%m/%Y")) & (as.Date("2007-02-03") > as.Date(pcdata[,1],format="%d/%m/%Y"))),]
-kwp <- as.numeric(pcdsub[,3])/1000
+kwp <- as.numeric(as.character(pcdsub[,3]))
 hist(kwp,xlab="Global Active Power (kilowatts)", col = "red", main = "Global Active Power")
 dev.off()
--- plot2.R
+++ plot2.R
@@ -2,9 +2,9 @@ library(graphics)
 png("plot2.png")
 pcdata <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
 pcdsub <- pcdata[which((as.Date("2007-01-31") < as.Date(pcdata[,1],format="%d/%m/%Y")) & (as.Date("2007-02-03") > as.Date(pcdata[,1],format="%d/%m/%Y"))),]
-kwp <- as.numeric(pcdsub[,3])/1000
+kwp <- as.numeric(as.character(pcdsub[,3]))
 timestamp <- paste(pcdsub$Date, " ", pcdsub$Time)
 df <- cbind(timestamp,kwp)
 colnames(df) <- c("Timestamp","Global_active_power")
-plot(strptime(df[,1],format="%d/%m/%Y %H:%M:%S"),df[,2],type="l", ylab="Global ACtive Power (kilowatts)", xlab="")
+plot(strptime(df[,1],format="%d/%m/%Y %H:%M:%S"),df[,2],type="l", ylab="Global Active Power (kilowatts)", xlab="")
 dev.off()
